6070a890507c18b9fa498c3bd053669b4d01aac5
Tom Spitz <tjspitz1986@gmail.com>
2023-01-19 17:28:22 -0600

    Added some routing functionality.

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
+const routes = require('./routes.js');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
-app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+// app.use(cors());
+app.use(express.json());
+app.use('/', routes);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
---
+++ 1-glossary/server/routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const controllers = require('./controllers');
+
+// router needs methods attached (once the methods exist)
+// router.get('/', controllers.get)
+// router.post('/', controllers.post)
+// router.delete('/', controllers.delete)
+
+module.exports = router;
