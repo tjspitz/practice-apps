c34c155be64fa8d4e09d22f140ccd6535ae94055
Tom Spitz <tjspitz1986@gmail.com>
2023-01-19 22:39:25 -0600

    Temp. de-separation of concerns/MVC. All methods in server/index,js and db/index.js for now.

---
+++ 1-glossary/server/models/index.js
@@ -0,0 +1,21 @@
+// const db = require('../db');
+
+// module.exports = {
+//   postWord: (word) => {
+//     // save a new word & descr DOCUMENT to the WORDS collection
+//   },
+
+//   updateWord: (word) => {
+//     // change existing word's details (aka client-side edit)
+//   },
+
+//   deleteWord: (word) => {
+//     // delete this DOCUMENT from the database
+
+//   },
+
+//   getAll: (wordList) => {
+//     // get the updated COLLECTION of WORDS from the database
+
+//   }
+// }
